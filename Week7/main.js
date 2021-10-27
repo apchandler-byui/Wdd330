e2b8c03330ac396088fbbdf1c439d46501d8d1fd
Andrew Chandler <achand4@wgu.edu>
2021-10-27 12:34:40 -0700

    Week 7

---
+++ Week7/main.js
@@ -0,0 +1,61 @@
+
+function longProcess() {
+    setTimeout(() => {
+       return 42
+     }, 200);
+    const total = 100 + longProcess();
+    console.log('total', total); // NaN
+ }
+
+ longProcess();
+ 
+function longProcess() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        if (1) {
+            resolve(42);
+        } else reject('failed');
+            }, 200);
+    });
+}
+longProcess()
+.then(result => {
+    let total = 100 + result;
+    console.log('total', total);
+})
+.catch(err => {
+    console.log(err);
+});
+
+
+function getJson(url) {
+    return fetch(url)
+        .then(response => {
+        if (response.ok) {
+            console.log('in then', response);
+            return response.json();
+        } else {
+            throw new Error('not ok');
+            console.log('will not run');
+        }
+        })
+        .catch(err => {
+        console.log(err);
+        });
+    }
+    
+
+const baseUrl = 'https://pokeapi.co/api/v2/';
+const myList = getJson(baseUrl + 'type/3');
+
+myList.then(data => {
+  console.log(data);
+  buildList(data);
+});
+
+function buildList(data) {
+  const myListElement = document.getElementById('list');
+  myListElement.innerHTML = data.pokemon
+    .map(item => `<li>${item.pokemon.name}</li>`)
+    .join('');
+}
